38cb0ec69bf6aeba3e4a4dec7aa1e89150de901a
Andrew <andrew-aralov@yandex.ru>
2025-08-07 11:02:30 +0200

    Add test for `optimal_preparation`.

---
+++ test_photon_catalysis/states.py
@@ -0,0 +1,34 @@
+import sympy as sp
+from photon_catalysis.utils import kets_to_state_dict
+
+
+all_states = {
+    'psi_1': kets_to_state_dict([(2, 0, 0), (0, 2, 0), (0, 0, 2)]),
+    'psi_2' : kets_to_state_dict([(3, 0, 0), (0, 3, 0), (0, 0, 3)]),
+    'psi_3' : kets_to_state_dict([(4, 0, 0), (0, 4, 0), (0, 0, 4)]),
+    'psi_4' : kets_to_state_dict([(2, 0, 0, 0), (0, 2, 0, 0), (0, 0, 2, 0), (0, 0, 0, 2)]),
+    'psi_5' : kets_to_state_dict([(0, 1, 2), (1, 2, 0), (2, 0, 1), (0, 2, 1), (1, 0, 2), (2, 1, 0)]),
+    'psi_6' : kets_to_state_dict([(1, 1, 0), (1, 0, 1), (0, 1, 1)]),
+    'psi_7' : kets_to_state_dict([(2, 2, 0), (2, 0, 2), (0, 2, 2)]),
+    'psi_8' : kets_to_state_dict([(2, 0, 0, 0), (0, 1, 1, 0), (0, 0, 0, 2)]),
+    'psi_9' : kets_to_state_dict([(3, 0, 0, 0), (0, 2, 1, 0), (0, 1, 2, 0), (0, 0, 0, 3)]),
+    'psi_10' : kets_to_state_dict([(0, 4, 0), (1, 2, 1), (2, 0, 2)]),
+    'psi_generic' : kets_to_state_dict([(3, 0, 0), (0, 3, 0), (0, 0, 3), (1, 1, 1)]),
+    'psi_supergeneric' : kets_to_state_dict([(2, 1, 0), (0, 2, 1)]),
+    'psi_subgeneric':
+        {
+             (3, 0, 0): sp.sqrt(13)/13,
+             (1, 2, 0): sp.sqrt(39)/13,
+             (1, 1, 1): sp.sqrt(78)/13,
+             (1, 0, 2): sp.sqrt(39)/13
+         },
+    'psi_K3' : {
+         (3, 0, 0, 0) : 1,
+         (2, 1, 0, 0) : 1,
+         (2, 0, 1, 0) : 1,
+         (2, 0, 0, 1) : 1,
+         (1, 1, 1, 0) : -1,
+         (1, 1, 0, 1) : -1,
+         (1, 0, 1, 1) : -1,
+         (0, 1, 1, 1) : -1 }
+}
---
+++ test_photon_catalysis/test_optimal_preparation.py
@@ -0,0 +1,37 @@
+import pytest
+
+from test_photon_catalysis.states import all_states
+from photon_catalysis.optimal_preparation import optimal_preparation
+
+
+expected_extra_photons = {
+    'psi_1': 1,
+    'psi_2': 1,
+    'psi_3': 2,
+    'psi_4': 2,
+    'psi_5': 1,
+    'psi_6': 1,
+    'psi_7': 1,
+    'psi_8': 2,
+    'psi_9': 2,
+    'psi_10': 2,
+    'psi_subgeneric': 1,
+    'psi_supergeneric': 1,
+    'psi_generic': 1,
+    'psi_K3': 2
+}
+assert(expected_extra_photons.keys() == all_states.keys())
+
+def get_input_dict():
+    r = {}
+    for k, v in all_states.items():
+        r[k] = (v, expected_extra_photons[k])
+    return r
+
+@pytest.mark.parametrize('state_name,tup', get_input_dict().items())
+def test_optimal_preparation(state_name, tup):
+    state, extra_photons = tup
+    mx_fid = 0
+    for _, _, fid in optimal_preparation(state, extra_photons, num_decompositions=2):
+        mx_fid = max(mx_fid, fid)
+    assert mx_fid > 0.999
