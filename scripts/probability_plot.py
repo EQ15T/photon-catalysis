ea48ab9ee7c29f2ac49b4157a39de35ee163746c
roseopicta <emilie.o.gillet+github@gmail.com>
2026-01-07 23:34:09 +0100

    Probability plot script uses benchmark_states

--- scripts/probability_plot.py
+++ scripts/probability_plot.py
@@ -7,63 +7,10 @@ import numpy as np
 import pandas as pd
 import sympy as sp
 
+from photon_catalysis.benchmark_states import benchmark_states_dict
 from photon_catalysis.optimal_preparation import optimal_preparation
 from photon_catalysis.utils import kets_to_state_dict, normalize_W, projection_prob
 from photon_catalysis.waring_preparation import waring_preparation
-
-all_states = {
-    "psi_1": kets_to_state_dict([(2, 0, 0), (0, 2, 0), (0, 0, 2)]),
-    "psi_2": kets_to_state_dict([(3, 0, 0), (0, 3, 0), (0, 0, 3)]),
-    "psi_3": kets_to_state_dict([(4, 0, 0), (0, 4, 0), (0, 0, 4)]),
-    "psi_4": kets_to_state_dict(
-        [(2, 0, 0, 0), (0, 2, 0, 0), (0, 0, 2, 0), (0, 0, 0, 2)]
-    ),
-    "psi_5": kets_to_state_dict(
-        [(0, 1, 2), (1, 2, 0), (2, 0, 1), (0, 2, 1), (1, 0, 2), (2, 1, 0)]
-    ),
-    "psi_6": kets_to_state_dict([(1, 1, 0), (1, 0, 1), (0, 1, 1)]),
-    "psi_7": kets_to_state_dict([(2, 2, 0), (2, 0, 2), (0, 2, 2)]),
-    "psi_8": kets_to_state_dict([(2, 0, 0, 0), (0, 1, 1, 0), (0, 0, 0, 2)]),
-    "psi_9": kets_to_state_dict(
-        [(3, 0, 0, 0), (0, 2, 1, 0), (0, 1, 2, 0), (0, 0, 0, 3)]
-    ),
-    "psi_10": kets_to_state_dict([(0, 4, 0), (1, 2, 1), (2, 0, 2)]),
-    "R4": kets_to_state_dict([(3, 0, 0), (0, 3, 0), (0, 0, 3), (1, 1, 1)]),
-    "R5": kets_to_state_dict([(2, 1, 0), (0, 2, 1)]),
-    "R2": {
-        (3, 0, 0): sp.sqrt(13) / 13,
-        (1, 2, 0): sp.sqrt(39) / 13,
-        (1, 1, 1): sp.sqrt(78) / 13,
-        (1, 0, 2): sp.sqrt(39) / 13,
-    },
-    "K3": {
-        (3, 0, 0, 0): 1,
-        (2, 1, 0, 0): 1,
-        (2, 0, 1, 0): 1,
-        (2, 0, 0, 1): 1,
-        (1, 1, 1, 0): -1,
-        (1, 1, 0, 1): -1,
-        (1, 0, 1, 1): -1,
-        (0, 1, 1, 1): -1,
-    },
-}
-expected_extra_photons = {
-    "psi_1": 1,
-    "psi_2": 1,
-    "psi_3": 2,
-    "psi_4": 2,
-    "psi_5": 1,
-    "psi_6": 1,
-    "psi_7": 1,
-    "psi_8": 2,
-    "psi_9": 2,
-    "psi_10": 2,
-    "R4": 1,
-    "R5": 1,
-    "R2": 1,
-    "K3": 2,
-}
-assert expected_extra_photons.keys() == all_states.keys()
 
 
 @dataclass
@@ -108,19 +55,10 @@ def prob_plot(num_decompositions=25) -> dict[str, list[ProbPlotRecord]]:
         return res
 
     res = {}
-    res["e1_psi_4"] = optimize_expr(make_expr_th1(all_states["psi_4"]))
-    res["e2_psi_4"] = optimize_expr(
-        make_expr_th2(all_states["psi_4"], expected_extra_photons["psi_4"])
-    )
-    res["e1_psi_8"] = optimize_expr(make_expr_th1(all_states["psi_8"]))
-    res["e2_psi_8"] = optimize_expr(
-        make_expr_th2(all_states["psi_8"], expected_extra_photons["psi_8"])
-    )
-    res["e1_psi_9"] = optimize_expr(make_expr_th1(all_states["psi_9"]))
-    res["e2_psi_9"] = optimize_expr(
-        make_expr_th2(all_states["psi_9"], expected_extra_photons["psi_9"])
-    )
-
+    for candidate in ["psi_4", "psi_8", "psi_9"]:
+        s = benchmark_states_dict[candidate]
+        res[f"e1_{s.name}"] = optimize_expr(make_expr_th1(s.state))
+        res[f"e2_{s.name}"] = optimize_expr(make_expr_th2(s.state, s.extra_photons))
     return res
 
 
